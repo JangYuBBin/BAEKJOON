39ff4a0f5581eacfc991ef89d6535a770681bef4
JangYuBBin <129468179+JangYuBBin@users.noreply.github.com>
2023-06-27 17:56:33 +0900

    Update 내리막 길.py

--- 동적 계획법/내리막 길.py
+++ 동적 계획법/내리막 길.py
@@ -1,28 +1,29 @@
 # 내리막길, 1520번, 백준
 
 # my thoughts :
-# 1. 어렵고 틀린 문제이므로 나중에 다시 풀어보아야 할 것 같습니다..!!
-# 2. By using DFS and Dp, We can solve it..!!
-# 3. dp[i][j] : (i, j)에서 (n - 1, m - 1)까지 내리막길로만 이동하는 경로의 갯수
-# 4. 결국 우리가 구해야 하는 것은 dp[0][0]..!!
+# 1. 그냥 단순하게 DFS를 활용한다면 시간초과를 받을 것입니다..!!
+# 2. 그래서 우리는 Dynamic Programming을 활용해야 합니다..!!
+# 3. dp[x][y] : (x, y)에서 (n - 1, m - 1)까지 내리막길로만 이동하는 경로의 갯수
+# 4. 결국 우리가 구해야 하는 것은 dp[0][0]입니다..!!
 
 import sys
-sys.setrecursionlimit(10**6)
 
 n, m = map(int, sys.stdin.readline().rstrip().split())
 
-arr = list()
+board = list()
 for _ in range(n):
-    arr.append(list(map(int, sys.stdin.readline().rstrip().split())))
+    board.append(list(map(int, sys.stdin.readline().rstrip().split())))
 
 dp = [[-1] * m for _ in range(n)]
 dp[n - 1][m - 1] = 1
+# 참고 > dp[x][y] = -1 : (x, y)에서 (n - 1, m - 1)까지 가는 경우의 수를 아직 구하지 못한 경우
+
 
 def dfs(cur_v):
     # Base Case
     if cur_v == (n - 1, m - 1):
         return dp[n - 1][m - 1]
-    
+
     # Visited Case
     if dp[cur_v[0]][cur_v[1]] != -1:
         return dp[cur_v[0]][cur_v[1]]
@@ -34,7 +35,7 @@ def dfs(cur_v):
         new_v = (cur_v[0] + d[0], cur_v[1] + d[1])
 
         if 0 <= new_v[0] < n and 0 <= new_v[1] < m:
-            if arr[new_v[0]][new_v[1]] < arr[cur_v[0]][cur_v[1]]:
+            if board[new_v[0]][new_v[1]] < board[cur_v[0]][cur_v[1]]:
                 dp[cur_v[0]][cur_v[1]] += dfs(new_v)
     
     return dp[cur_v[0]][cur_v[1]]
