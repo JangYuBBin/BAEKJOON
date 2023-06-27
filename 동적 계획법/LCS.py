c618be323ea120e8cbbc9e23bb169db02b6293f6
JangYuBBin <129468179+JangYuBBin@users.noreply.github.com>
2023-06-27 17:57:41 +0900

    Update LCS.py

--- 동적 계획법/LCS.py
+++ 동적 계획법/LCS.py
@@ -2,8 +2,10 @@
 
 # my thoughts :
 # 1. By using Dynamic Programming, We can solve it..!!
-# 2. dp[i][j] : str1의 i번째 인덱스, str2의 j번째 인덱스까지 비교했을 때 나올 수 있는 부분 수열 중 가장 긴 것의 길이
-# 3. 결국 우리가 구해야하는 것은 dp[len1 - 1][len2 - 1]입니다..!!
+# 2. dp[i][j] : str1의 i번째 인덱스, str2의 j번째 인덱스까지 확인하였을 때 나올 수 있는 LCS(최장 공통 부분 수열)의 길이
+# 3. 결국 우리가 구해야 하는 것은 dp[len(str1) - 1][len(str2) - 1]입니다..!!
+# 4. 만약 str1[i] == str2[j]라면 dp[i][j] = dp[i - 1][j - 1] + 1
+# 5. 만약 str1[i] != str2[j]라면 dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])입니다..!! <<-- "이 부분은 약간의 센스가 필요했던 것 같습니다..!!"
 
 import sys
 
@@ -14,7 +16,7 @@ len1 = len(str1)
 len2 = len(str2)
 
 dp = [[0] * len2 for _ in range(len1)]
-# 초깃값 설정
+# 초깃값 설정 <<-- "참고로 Dp 배열을 만드는데 있어서 초깃값 설정 작업도 매우 중요한 작업이라고 할 수 있습니다..!!"
 checkFlag = False
 
 for i in range(0, 0 + 1, 1):
@@ -49,7 +51,7 @@ for i in range(1, len1, 1):
     for j in range(1, len2, 1):
         if str1[i] == str2[j]:
             dp[i][j] = dp[i - 1][j - 1] + 1
-        else:
+        elif str1[i] != str2[j]:
             dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
 
 print(dp[len1 - 1][len2 - 1])
