959cf12084b150abaaed051638b89dd7e3b2c6f0
JangYuBBin <129468179+JangYuBBin@users.noreply.github.com>
2023-06-27 18:00:06 +0900

    Update 평범한 배낭.py

--- 동적 계획법/평범한 배낭.py
+++ 동적 계획법/평범한 배낭.py
@@ -1,24 +1,26 @@
 # 평범한 배낭, 12865번, 백준
 
 # my thoughts :
-# 1. 틀렸던 문제이고 어려운 문제이므로 다음에 다시 풀면 좋을 듯 합니다..!!
-# 2. dp[i][j] : i번째 인덱스의 물건까지 확인했을 때 무게제한이 j인 가방에 담을 수 있는 물건들의 가치의 최대합
-# 3. 그렇다면 결국 우리가 구해야 하는 것은 dp[n - 1][k]입니다.
+# 1. 아주 유명한 냅색 문제(Knapsack Problem) or 0 - 1 배낭문제라고 합니다.
+# 2. By using Dynamic Programming, We can solve it..!!
+# 3. dp[i][j] : i번째 물건까지 확인하였을 때 무게제한이 j인 가방에 들어갈 수 있는 물건들의 가치합의 최대합
+# 4. 결국 우리가 구해야 하는 것은 dp[n][k]입니다..!!
+# 5. 다음에 다시 풀어보면 좋을 문제인 것 같습니다..!!
 
 import sys
 
 n, k = map(int, sys.stdin.readline().rstrip().split())
 
-arr = list()
+arr = [(0, 0)]
 
 for _ in range(n):
     w, v = map(int, sys.stdin.readline().rstrip().split())
 
     arr.append((w, v))
 
-dp = [[0] * (k + 1) for _ in range(n)]
+dp = [[0] * (k + 1) for _ in range(n + 1)]
 
-for i in range(0, n, 1):
+for i in range(1, n + 1, 1):
     w = arr[i][0] # 현재 확인하고 있는 물건의 무게
     v = arr[i][1] # 현재 확인하고 있는 물건의 가치
 
@@ -28,4 +30,4 @@ for i in range(0, n, 1):
         elif j >= w:
             dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - w] + v)
 
-print(dp[n - 1][k])
+print(dp[n][k])
